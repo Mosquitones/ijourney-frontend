09481aa48691a8e19f0ae14ae7acd5ad0ad002c2
Isaque J Souza <67837824+lsaque@users.noreply.github.com>
2023-10-02 19:30:05 -0300

    Update Users.id.notifications.read.ts

--- src/services/api/lib/users/Users.id.notifications.read.ts
+++ src/services/api/lib/users/Users.id.notifications.read.ts
@@ -3,7 +3,7 @@ import { ApiResponseTypes, UserTypes, api } from 'services'
 import { NotificationResponseTypes } from './Users.id.notifications.types'
 
 const readAllNotifications = async (userId: UserTypes['id']) => {
-  const { data: response } = await api.get<
+  const { data: response } = await api.post<
     ApiResponseTypes<NotificationResponseTypes>
   >(`/users/${userId}/notifications/read`)
 
